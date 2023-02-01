180e9f0310fd7058caa95279362aa21546f52cf7
Taras Martyniuk <bythewaters800@gmail.com>
2023-02-01 15:27:41 +0100

    Add model FeedBack & add feature in Customer

--- delivery/models.py
+++ delivery/models.py
@@ -13,3 +13,25 @@ class Customer(AbstractUser):
 
     def get_absolute_url(self):
         return reverse("taxi:driver-detail", kwargs={"pk": self.pk})
+
+    def __str__(self):
+        return (
+            f"{self.first_name} "
+            f"{self.last_name} "
+            f"{self.email} "
+            f"{self.address} "
+            f"{self.phone_number}"
+        )
+
+
+class FeedBack(models.Model):
+    comment = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True)
+    customer = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        related_name="feedback",
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        ordering = ["created_time"]

4a8a13bc4faab1f15aba2b09a82e1af8871be1d1
Taras Martyniuk <bythewaters800@gmail.com>
2023-02-01 15:33:21 +0100

    Add admin register Customer in admin.py

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from delivery.models import Customer
+
+
+@admin.register(Customer)
+class CustomerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("address", "phone_number")
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("address", "phone_number")}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "address",
+                        "phone_number"
+                    )
+                },
+            ),
+        )
+    )

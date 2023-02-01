98f2a4bd732c459ff8ad4d25b3e93a830baad6db
Taras Martyniuk <bythewaters800@gmail.com>
2023-02-01 15:24:23 +0100

    Add model Customer in models.py

--- delivery/models.py
+++ delivery/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Customer(AbstractUser):
+    address = models.CharField(max_length=255, blank=False, null=False)
+    phone_number = models.CharField(max_length=255, blank=False, null=False)
+
+    class Meta:
+        verbose_name = "customer"
+        ordering = ["username"]
+
+    def get_absolute_url(self):
+        return reverse("taxi:driver-detail", kwargs={"pk": self.pk})

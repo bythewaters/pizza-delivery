949a7ce18e54c34fb6ff11d5bea5a39da4772abe
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-01 12:53:46 +0100

    Change: django_debug in settings

--- delivery_pizza/settings.py
+++ delivery_pizza/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = "RENDER" not in os.environ
+DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1"]
 INTERNAL_IPS = [

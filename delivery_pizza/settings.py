82e5e571b2af2a2db157b2ee6ccef73507983537
Taras Martyniuk <bythewaters800@gmail.com>
2023-03-02 15:33:29 +0100

    Get .env variables with dotenv

--- delivery_pizza/settings.py
+++ delivery_pizza/settings.py
@@ -12,18 +12,23 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
+SECRET_KEY = os.getenv(
+    "DJANGO_SECRET_KEY"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
+DEBUG = os.getenv("DJANGO_DEBUG") != "False"
 
 ALLOWED_HOSTS = ["127.0.0.1", "pizza-delivery-ixg9.onrender.com"]
 INTERNAL_IPS = [
